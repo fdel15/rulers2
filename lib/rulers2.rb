2276e2481bff4edcaccf03fba4b90a5eea3a7177
Frank DelPidio <frankdaedalus@gmail.com>
2015-11-02 10:51:59 -0500

    add application class to Rulers module

--- lib/rulers2.rb
+++ lib/rulers2.rb
@@ -1,5 +1,9 @@
 require "rulers2/version"
 
 module Rulers2
-  # Your code goes here...
+  class Application
+    def call(env)
+      [200, {'Content-Type' => 'text/html'}, ["Hello from Ruby on Rulers!"]]
+    end
+  end
 end
