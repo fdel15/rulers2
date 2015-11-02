afad58e2529e9282cf882d5974cc7bc903bfab88
Frank DelPidio <frankdaedalus@gmail.com>
2015-11-02 11:20:25 -0500

    add debugging file

--- lib/rulers2.rb
+++ lib/rulers2.rb
@@ -1,8 +1,10 @@
 require "rulers2/version"
 
+
 module Rulers2
   class Application
     def call(env)
+      `echo debug > debug.txt`;
       [200, {'Content-Type' => 'text/html'}, ["Hello from Ruby on Rulers!"]]
     end
   end
