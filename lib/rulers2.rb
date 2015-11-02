3fa08cb3e94efffd4ecc45dee54e86fc17913ef1
Frank DelPidio <frankdaedalus@gmail.com>
2015-11-02 13:05:02 -0500

    hack favicon error

--- lib/rulers2.rb
+++ lib/rulers2.rb
@@ -5,6 +5,9 @@ require "rulers2/version"
 module Rulers2
   class Application
     def call(env)
+      if env['PATH_INFO'] == '/favicon.ico'
+        return [404, {'Content-Type' => 'text/html'}, []]
+      end
       klass, act = get_controller_and_action(env)
       controller = klass.new(env)
       text = controller.send(act)
