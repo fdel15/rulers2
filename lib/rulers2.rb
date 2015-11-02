95ff43793b07f87e2b33219bd6380c9fec3f110b
Frank DelPidio <frankdaedalus@gmail.com>
2015-11-02 12:41:33 -0500

    create controller class

--- lib/rulers2.rb
+++ lib/rulers2.rb
@@ -1,11 +1,24 @@
-require "rulers2/version"
 require "rulers2/array"
+require "rulers/routing"
+require "rulers2/version"
 
 module Rulers2
   class Application
     def call(env)
-      `echo debug > debug.txt`;
-      [200, {'Content-Type' => 'text/html'}, ["Hello from Ruby on Rulers!"]]
+      klass, act = get_controller_and_action(env)
+      controller = klass.new(env)
+      text = controller.send(act)
+      [200, {'Content-Type' => 'text/html'}, [text]]
+    end
+  end
+
+  class Controller
+    def initialize(env)
+      @env = env
+    end
+
+    def env
+      @env
     end
   end
 end
