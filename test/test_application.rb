faeae33077945703a70e5f5a0b485119f13d159a
Frank DelPidio <frankdaedalus@gmail.com>
2015-11-02 15:54:08 -0500

    update tests

--- test/test_application.rb
+++ test/test_application.rb
@@ -1,7 +1,17 @@
 require_relative "test_helper"
 
-class TestApp < Rulers2::Application
+class TestController < Rulers2::Controller
+  def index
+    "Hello!"
+  end
 end
+
+class TestApp < Rulers2::Application
+  def get_controller_and_action(env)
+    [TestController, "index"]
+  end
+end
+
 
 class RulersAppTest < Test::Unit::TestCase
   include Rack::Test::Methods
@@ -11,7 +21,7 @@ class RulersAppTest < Test::Unit::TestCase
   end
 
   def test_request
-    get "/"
+    get "/example/route"
 
     assert last_response.ok?
     body = last_response.body
