fcfb8bf7b21927c6a2189a240f5ccc201b62addf
Frank DelPidio <frankdaedalus@gmail.com>
2015-11-03 15:53:06 -0500

    add methods to sqlite_model

--- lib/rulers2/sqlite_model.rb
+++ lib/rulers2/sqlite_model.rb
@@ -7,6 +7,39 @@ module Rulers2
   module Model
     class SQLite
 
+      def initialize(data = nil)
+        @hash = data
+      end
+
+      def self.to_sql(val)
+        case val
+        when Numeric
+          val.to_s
+        when String
+          "'#{val}'"
+        else
+          raise "Can't change #{val.class} to SQL!"
+        end
+      end
+
+      def self.create(values)
+        values.delete "id"
+        keys = schema.keys - ["id"]
+        vals = keys.map do |key|
+          values[key] ? to_sql(values[key]) : "null"
+        end
+
+        DB.execute <<-SQL
+          INSERT INTO #{table} (#{keys.join ","})
+            VALUES (#{vals.join ","});
+        SQL
+
+        data = Hash[keys.zip vals]
+        sql = "SELECT last_insert_rowid();"
+        data["id"] = DB.execute(sql)[0][0]
+        self.new data
+      end
+
       def self.table
         Rulers2.to_underscore name
       end
@@ -18,6 +51,12 @@ module Rulers2
           @schema[row["name"]] = row["type"]
         end
         @schema
+      end
+
+      def self.count
+        DB.execute(<<-SQL)[0][0]
+          SELECT COUNT(*) FROM #{table}
+        SQL
       end
     end
   end
